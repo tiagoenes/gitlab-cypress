a57c03660db5a4dd312a97faa780ada393d8ad18
tiagoenes <enes.tiago@gmail.com>
2022-11-07 17:06:05 +0100

    first commit

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,35 @@
+export const loginTest = () => {
+  describe('login', () => {
+    it('passes', () => {
+      cy.visit('https://prisma-it.tst.prisma-it.com/usermanager:authentication/login')
+    })
+    it('should have teo inputs', () => {
+      cy.get('#emailaddress').should('be.visible')
+      cy.get('#password').should('be.visible')
+    });
+    it('should have a login button', () => {
+      cy.get('#userformsubmit').should('be.visible')
+    });
+    it('should be able to login', () => {
+      cy.get('#emailaddress').type('bla@prisma-it.com')
+      cy.get('#password').type('bla')
+      // cy.get('#userformsubmit').click()
+      Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+    })
+      cy.get('h2').contains('WELKOM')
+      cy.get('.menuWelcomePage div').first().click()
+      
+      // cy.intercept({
+      //     method: "GET",
+      //     url: "/BlazeDS/editor/user/currentUser",
+      //   }).as("dataGetFirst");
+      // cy.wait("@dataGetFirst").its('response.statusCode').should('equal', 400);
+      //   cy.reload()
+      });
+  })
+  }
+  
+  loginTest()
